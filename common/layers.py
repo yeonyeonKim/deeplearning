0f90fcdccf5169dadefaf46147d977fa674d681d
yeonyeonKim <tmddus5342@naver.com>
2020-07-27 22:12:35 +0900

    20.07.27

---
+++ common/layers.py
@@ -0,0 +1,63 @@
+import numpy as np
+from common.function import *
+
+class Relu:
+    #mask변수는 true와 false로 된 넘파이배열
+    def __init__(self):
+        self.mask = None
+    def forward(self,x):
+        #x의 값이 0이하면 true
+        self.mask = (x<=0)
+        out = x.copy()
+        out[self.mask] = 0
+        return out
+    def backward(self,dout):
+        dout[self.mask] = 0
+        dx = dout
+        return dx
+class Sigmoid:
+    def __init__(self):
+        self.out = None
+    def forward(self,x):
+        out = 1/(1+np.exp(-x))
+        self.out = out
+        return out
+
+    def backward(self, dout):
+        dx = dout *(1.0-self.out)*self.out
+        return dx
+
+class Affine:
+    def __init__(self,W,b):
+        self.W = W
+        self.b = b
+        self.x =None
+        self.dW = None
+        self.db = None
+    def forward(self,x):
+        self.x = x
+        out = np.dot(x,self.W)+self.b
+        return out
+    def backward(self,dout):
+        dx = np.dot(dout,self.W.T)
+        self.dW = np.dot(self.x.T,dout)
+        self.db = np.sum(dout,axis=0)
+        return dx
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None
+        self.y = None
+        self.t = None
+
+    def forward(self,x,t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y,self.t)
+        return self.loss
+
+    def backward(self,dout):
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t)/batch_size
+        return dx
+
